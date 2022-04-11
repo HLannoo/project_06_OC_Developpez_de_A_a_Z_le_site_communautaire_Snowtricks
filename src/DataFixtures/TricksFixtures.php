a80b4d0bfd2c09c49c27925213a2faf259381e94
Hédi Lannoo <hedilannoo@gmail.com>
2022-04-11 19:55:52 +0200

    add first version function add-page

--- src/DataFixtures/TricksFixtures.php
+++ src/DataFixtures/TricksFixtures.php
@@ -4,10 +4,10 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Tricks;
-use App\Entity\Images;
-use App\Entity\Videos;
-use App\Entity\Comments;
+use App\Entity\Trick;
+use App\Entity\Image;
+use App\Entity\Video;
+use App\Entity\Comment;
 use App\Entity\Category;
 
 class TricksFixtures extends Fixture
@@ -18,17 +18,16 @@ class TricksFixtures extends Fixture
         //créer 3 catégories
         for ($i=1; $i <= 3; $i++) {
             $category = new Category();
-            $category->setTitle("Catégorie n°$i")
-                ->setDescription("Description n°$i");
+            $category->setName("Catégorie n°$i");
 
             $manager->persist($category);
 
             // Créer 10 tricks
             for ($u = 1; $u <= 10; $u++) {
-                $tricks = new Tricks();
-                $tricks->setTitle("titre de l'article n°$u")
-                    ->setContent("Contenu de l'article n°$u")
-                    ->setCreatedAt(new \DateTimeImmutable())
+                $tricks = new Trick();
+                $tricks->setName("titre de l'article n°$u")
+                    ->setDescription("Contenu de l'article n°$u")
+                    ->setCreatedAt(new \DateTime())
                     ->setCategory($category);
 
                 $manager->persist($tricks);
@@ -36,13 +35,12 @@ class TricksFixtures extends Fixture
                 // Ajout des commentaires à l'article
                 for ($k = 1; $k <= mt_rand(1, 4); $k++) {
 
-                    $comment = new Comments();
+                    $comment = new Comment();
 
-                    $now = new \DateTimeImmutable();
+                    $now = new \DateTime();
 
-                    $comment->setAuthor('Hédi Lannoo')
-                        ->setContent("Commentaire de l'article n°$u")
-                        ->setCreateAt($now)
+                    $comment->setContent("Commentaire de l'article n°$u")
+                        ->setCreatedAt($now)
                         ->setTrick($tricks);
 
                     $manager->persist($comment);
@@ -50,18 +48,19 @@ class TricksFixtures extends Fixture
                     // Ajout des images à l'article
                     for ($l = 1; $l <= mt_rand(1, 4); $l++) {
 
-                        $image = new Images();
+                        $image = new Image();
                         $image->setPath('https://via.placeholder.com/130.png/09f/fff')
-                            ->setIdTrick($tricks);
+                            ->setName('nom image $l')
+                            ->setTrick($tricks);
 
                         $manager->persist($image);
 
                         // Ajout des vidéos à l'article
                         for ($v = 1; $v <= mt_rand(1, 4); $v++) {
 
-                            $video = new Videos();
-                            $video->setPath('https://via.placeholder.com/130.png/09f/fff')
-                                ->setIdTrick($tricks);
+                            $video = new Video();
+                            $video->setUrl('https://via.placeholder.com/130.png/09f/fff')
+                                ->setTrick($tricks);
 
                             $manager->persist($video);
                         }
