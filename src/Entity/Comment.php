0672974ebd718fd1594c36374fb708f6d2518fc1
Hédi Lannoo <hedilannoo@gmail.com>
2022-05-02 20:21:02 +0200

    add security controller logout and twig display edit

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -22,9 +22,6 @@ class Comment
     #[ORM\ManyToOne(targetEntity: Trick::class, inversedBy: 'comments')]
     private $trick;
 
-    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'comments')]
-    private $user;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -66,15 +63,4 @@ class Comment
         return $this;
     }
 
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): self
-    {
-        $this->user = $user;
-
-        return $this;
-    }
 }
